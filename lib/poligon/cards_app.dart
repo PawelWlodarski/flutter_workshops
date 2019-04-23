233950ab3516bb34611afec7e80d74b621bdc121
PawelWlodarski <p.wlodarski@oberthur.com>
2019-04-23 11:39:37 +0200

    changing leading icon according to tile expansion

--- lib/poligon/cards_app.dart
+++ lib/poligon/cards_app.dart
@@ -29,22 +29,12 @@ class CardsPanel extends StatefulWidget {
 }
 
 class _CardsPanelState extends State<CardsPanel> {
-
   _itemBusinessFilter(MyItem item) => item.id % 4 == 0;
 
-  List<Widget> _buildCards() => widget._store
-      .items()
-      .map((item) => ExpansionTile(
-            key: Key("card${item.id}"),
-            initiallyExpanded: _itemBusinessFilter(item),
-            leading: Icon(Icons.arrow_downward),
-            title: Text(item.title, style: TextStyle(fontWeight: FontWeight.bold)),
-            children: <Widget>[
-              Text(item.content),
-              Container(alignment: Alignment.centerRight, child: IconButton(icon: Icon(Icons.link), onPressed: () {}))
-            ],
-          ))
-      .toList();
+  List<Widget> _buildCards() => widget._store.items().map((item) {
+        final isExpanded = _itemBusinessFilter(item);
+        return _ItemTile(item,isExpanded);
+      }).toList();
 
   @override
   Widget build(BuildContext context) => ListView(
@@ -52,13 +42,55 @@ class _CardsPanelState extends State<CardsPanel> {
       );
 }
 
+class _ItemTile extends StatefulWidget{
+
+  final MyItem _item;
+  final bool _isExpanded;
+
+  _ItemTile(this._item, this._isExpanded) : super();
+
+  @override
+  _ItemTileState createState() => _ItemTileState(_isExpanded);
+}
+
+class _ItemTileState extends State<_ItemTile> {
+
+  bool _isExpanded;
+
+  _ItemTileState(bool isExpanded){
+    this._isExpanded = isExpanded;
+  }
+
+  _itemIcon() => this._isExpanded ? Icon(Icons.arrow_upward) : Icon(Icons.arrow_downward);
+
+  @override
+  Widget build(BuildContext context) => ExpansionTile(
+    initiallyExpanded: widget._isExpanded,
+    leading: _itemIcon(),
+    onExpansionChanged: (isExpanding){
+        setState((){this._isExpanded = isExpanding;});
+    },
+    title: Text(widget._item.title, style: TextStyle(fontWeight: FontWeight.bold)),
+    children: <Widget>[
+      Text(widget._item.content),
+      Container(
+          alignment: Alignment.centerRight,
+          child: IconButton(
+            icon: Icon(Icons.link),
+            onPressed: () {},
+          ))
+    ],
+  );
+}
+
+
 abstract class ItemsStore {
   Iterable<MyItem> items();
 }
 
 class LocalItemsStore implements ItemsStore {
-  Iterable<MyItem> items()  =>
-      Iterable<int>.generate(10).map((i) => MyItem(i,"item$i", List.filled(5, "lorem ipsum $i ").join()));
+  Iterable<MyItem> items() =>
+      Iterable<int>.generate(10).map((i) => MyItem(i, "item$i", List.filled(5, "lorem ipsum $i ").join()));
 }
 
 class MyItem {
@@ -66,5 +98,5 @@ class MyItem {
   final String title;
   final String content;
 
-  MyItem(this.id,this.title, this.content);
+  MyItem(this.id, this.title, this.content);
 }
